ec85e19bc31da7d9fd27ced269303ae161bf3466
Roberto dos Santos <robertosantos9091@gmail.com>
2021-08-23 16:37:19 -0300

    Teste final2

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 console.log(process.env.DATABASE_URL)
 module.exports={
     dialect: "postgres",
-    url: process.env.POST,
+    url: process.env.DATABASE_URL,
     //host: "localhost",
     //port: "5432",
     //username: "postgres",
