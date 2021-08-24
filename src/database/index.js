a0797c3c02d8dcfd1cbd4f95d4632649014a64fc
Roberto dos Santos <robertosantos9091@gmail.com>
2021-08-23 23:36:07 -0300

    Teste final2

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,12 @@
-//require('dotenv').config();
+require('dotenv').config();
 const Sequelize=require('sequelize')
 const config=require("../config/database")
 const Categoria=require('../model/Categoria')
 const Questao=require('../model/Questao')
 const Tema=require('../model/Tema')
-const conexao=new Sequelize(process.env.POST);
+const conexao=new Sequelize(process.env.DATABASE_URL);
 
-//dotenv.config();
+dotenv.config();
 
 Categoria.init(conexao);
 Questao.init(conexao);
